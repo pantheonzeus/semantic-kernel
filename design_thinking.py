dc8fa6d9ee26ba3cc9893260b96176ecd79c53d1
pantheonzeus <christoph.nuetzel@futurice.com>
2023-12-11 16:30:33 +0100

    Clean up

--- design_thinking.py
+++ design_thinking.py
@@ -7,9 +7,12 @@ from semantic_kernel.planning.sequential_planner.sequential_planner_config impor
 
 import customer_feedback
 
-kernel = sk.Kernel()
-
+pluginsDirectory = "./plugins-sk"
 useAzureOpenAI = False
+customer_feedback = customer_feedback.customer_feedback
+
+# Connect to OpenAPI or Azure OpenAPI. Then load the planner and import the skills
+kernel = sk.Kernel()
 
 if useAzureOpenAI:
     deployment, api_key, endpoint = sk.azure_openai_settings_from_dot_env()
@@ -18,15 +21,12 @@ else:
     api_key, org_id = sk.openai_settings_from_dot_env()
     kernel.add_text_completion_service("openai", OpenAIChatCompletion("gpt-3.5-turbo-0301", api_key, org_id))
 
+planner = SequentialPlanner(kernel, SequentialPlannerConfig(excluded_skills=["this"]))
+kernel.import_semantic_skill_from_directory(pluginsDirectory, "DesignThinking")
+
 print("A kernel is now ready.")
 
-pluginsDirectory = "./plugins-sk"
-
-customer_feedback = customer_feedback.customer_feedback
-
-planner = SequentialPlanner(kernel, SequentialPlannerConfig(excluded_skills=["this"]))
-
-kernel.import_semantic_skill_from_directory(pluginsDirectory, "DesignThinking")
+# Run Design Thinking Process around the provided customer feedback
 
 ask = 'This is customer feedback. Emphasize it, Define it. Ideate.'
 plan = asyncio.run(planner.create_plan_async(goal=ask))
@@ -34,7 +34,5 @@ result = asyncio.run(plan.invoke_async(input=customer_feedback))
 
 for index, step in enumerate(plan._steps):
     print(f"✅ Step {index+1} used function `{step._function.name}`")
-
-trace_resultp = True
 
 print("## ✨ Generated result from the ask: " + str(ask) +  "\n\n---\n"  + str(result))
